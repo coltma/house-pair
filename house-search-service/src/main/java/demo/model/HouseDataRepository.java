04dd85cb9f9c50042de36b59bdd9c2c9ccc06353
coltma <shawma.dev@gmail.com>
2017-12-19 12:31:32 -0800

    simple hint

--- house-search-service/src/main/java/demo/model/HouseDataRepository.java
+++ house-search-service/src/main/java/demo/model/HouseDataRepository.java
@@ -3,4 +3,6 @@ package demo.model;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface HouseDataRepository extends PagingAndSortingRepository<HouseData, String> {
+    // additional custom finder methods go here
+
 }
