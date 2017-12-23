b53591bca98505f95b3cfb4a4ed411f8061220f4
coltma <shawma.dev@gmail.com>
2017-12-23 15:06:30 -0800

    circular breaks with house-subscription-service: to produce message.

--- spring-cloud-house-pair/house-subscription-service/src/main/java/demo/service/impl/MQServiceImpl.java
+++ spring-cloud-house-pair/house-subscription-service/src/main/java/demo/service/impl/MQServiceImpl.java
@@ -2,15 +2,18 @@ package demo.service.impl;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import demo.model.HouseNotification;
 import demo.service.MQService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
+@EnableCircuitBreaker
 public class MQServiceImpl implements MQService {
 
     @Autowired
@@ -19,11 +22,16 @@ public class MQServiceImpl implements MQService {
     @Autowired
     private ObjectMapper objectMapper;
 
+    @HystrixCommand(fallbackMethod = "produceNotificationFallBack")
     @Override
     public void produce(HouseNotification notification) throws JsonProcessingException {
         log.info(String.format("Before send: %s", notification.getEmailAddress()));
         this.rabbitTemplate.convertAndSend("direct_email_server",
                 "q_email_servers",
                  objectMapper.writeValueAsString(notification));
+    }
+
+    public void produceNotificationFallBack(HouseNotification notification) {
+        log.error("Hystrix Fallback Method. Unable to send message for notification.");
     }
 }
