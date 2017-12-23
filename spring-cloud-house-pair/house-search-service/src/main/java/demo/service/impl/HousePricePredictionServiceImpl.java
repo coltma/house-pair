b4bf224bd4007a152b23d0b5d23c132828a4d9aa
coltma <shawma.dev@gmail.com>
2017-12-23 15:05:35 -0800

    circular breaks with house-search-service: REST call to machine learning service.

--- spring-cloud-house-pair/house-search-service/src/main/java/demo/service/impl/HousePricePredictionServiceImpl.java
+++ spring-cloud-house-pair/house-search-service/src/main/java/demo/service/impl/HousePricePredictionServiceImpl.java
@@ -1,24 +1,31 @@
 package demo.service.impl;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import demo.model.PredictedPrice;
 import demo.service.HousePricePredictionService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 @Slf4j
+@EnableCircuitBreaker
 public class HousePricePredictionServiceImpl implements HousePricePredictionService {
     private static final String predictUrl = "http://localhost:8000/prediction/";
 
     private RestTemplate rawRestTemplate = new RestTemplate();
 
-    // @HystrixCommand(fallbackMethod = "xxx")
+    @HystrixCommand(fallbackMethod = "getPredictPriceFallBack")
     @Override
     public PredictedPrice predict(double bedroom, double bathroom, double areaSize) {
         String params = String.format("?br=%f&ba=%f&area=%f", bedroom, bathroom, areaSize);
         log.info(String.format("Calling django REST API"));
         return this.rawRestTemplate.getForObject(predictUrl + params, PredictedPrice.class);
+    }
+
+    public void getPredictPriceFallBack(double bedroom, double bathroom, double areaSize) {
+        log.error("Hystrix Fallback Method. Unable to send house feature to django.");
     }
 }
